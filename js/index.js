78e2f820387fa0823f32e3628f3b3c275af27344
Jeremiah <kwagh.jeremiah@gmail.com>
2023-10-16 16:59:46 -0400

    js updates made the cards show up

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
 let searchTerm = document.getElementById("form");
 const searchButton = document.querySelector("button");
+let searchResults = [];
+const header = document.getElementById("header-background");
+
 
 //GET beer
 const getBeer = () => {
-    fetch("https://api.punkapi.com/v2/beers/random")
+    fetch("https://api.punkapi.com/v2/beers?page=1&per_page=80")
         .then((resp) => resp.json())
         .then((data) => {
             console.log(data);
@@ -17,35 +20,45 @@ const getBeer = () => {
 const findBeer = (beer) => {
     const beerImg = document.createElement("img");
     const beerName = document.createElement("h4");
+    const beerAbv = document.createElement("p");
     const beerTagLine = document.createElement("p");
+    const beerContainer = document.createElement("div");
     const beerCard = document.createElement("div");
-    beerImg.src = beer.image_url;
+    beerCard.classList.add("card");
+    beerContainer.classList.add("container");
+    beerImg.classList.add("beerPhoto");
 
+
+    beerImg.src = beer.image_url;
+    beerAbv.innerText = "ABV: " + beer.abv + "%";
     beerTagLine.innerText = beer.tagline;
+    beerName.innerText = beer.name;
 
     let text = String(beerTagLine.innerText);
     
-    console.log(text);
-    
-    //search button works - does not reset for new terms in search bar
+//    console.log(text);
+//search button works
     searchButton.addEventListener("click", () => {
         let test = text.indexOf(searchTerm.value);
         if (test != -1) {
-            console.log("TRUE");
+            console.log(beerName);
+            console.log(beer.id);
+            console.log(beerTagLine);
         } else {
             console.log("FALSE");
         };
-
     });
-    
+
+//Create new card
+
+    beerContainer.appendChild(beerName);
+    beerContainer.appendChild(beerAbv);
+    beerContainer.appendChild(beerTagLine);
+    beerContainer.appendChild(beerImg);
+    beerCard.appendChild(beerContainer);
+    header.appendChild(beerCard);
 };
 
-//search through the beer
-/* const search = () => {
-    const input = 
-}; */
-
-
-
-
 getBeer();
+
+console.log(searchResults);    
