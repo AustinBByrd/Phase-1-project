abf2cb28ff873d89c54aeddca8c996f9df6aaf73
Austin Byrd <byrda05@gmail.com>
2023-10-16 16:54:17 -0500

    centered beerphoto, wrapped cards

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ const header = document.getElementById("header-background");
 
 //GET beer
 const getBeer = () => {
-    fetch("https://api.punkapi.com/v2/beers?page=4&per_page=80")
+    fetch("https://api.punkapi.com/v2/beers?page=2&per_page=80")
         .then((resp) => resp.json())
         .then((data) => {
             console.log(data);
