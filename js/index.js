09f954f36a90fd587202192b60cdb7d2a7d1c268
Jeremiah <kwagh.jeremiah@gmail.com>
2023-10-16 14:05:31 -0400

    JS updates

--- js/index.js
+++ js/index.js
@@ -1,16 +1,23 @@
 const getBeer = () => {
-    fetch("https://api.punkapi.com/v2/beers")
+    fetch("https://api.punkapi.com/v2/beers/random")
         .then((resp) => resp.json())
         .then((beer) => {
             console.log(beer);
+            findBeer(beer);
         });
     };
     
     
-/*    const findBeer = (beer) => {
-        const beerImg = document.createAttribute("img");
+    const findBeer = (beer) => {
+        const beerImg = document.createElement("img");
+        const beerName = document.createElement("h4");
+        const beerTagLine = document.createElement("p");
+        const beerCard = document.createElement("div");
         beerImg.src = beer.image_url;
+        beerTagLine.innerText = beer.tagline;
     
-        console.log(beerImg);
+        console.log(beerTagLine);
     };
-*/
+
+
+getBeer();
