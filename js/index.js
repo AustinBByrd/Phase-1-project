f7ae8955edc2c4c7191ac580cfe451c73ddb7f86
Gabriel Wortmann <wortmanngabriel@gmail.com>
2023-10-17 12:24:37 -0500

    bugs fixed and random button works

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ let searchTerm = document.getElementById("form");
 const searchButton = document.querySelector("button");
 let searchResults = [];
 const header = document.getElementById("header-background");
+const randomButton = document.getElementById("random");
 
 
 //GET beer
@@ -16,7 +17,6 @@ const getBeer = () => {
         });
     };
     
-
 //pull beer and grab tagline
 const findBeer = (beer) => {
     const beerImg = document.createElement("img");
@@ -38,7 +38,6 @@ const findBeer = (beer) => {
 
     let text = String(beerTagLine.innerText);
     
-
 //    console.log(text);
 //search button works
     searchButton.addEventListener("click", () => {
@@ -55,7 +54,6 @@ const findBeer = (beer) => {
         };
     });
 
-
 //Create new card
     // beerContainer.appendChild(beerName);
     // beerContainer.appendChild(beerAbv);
@@ -65,7 +63,46 @@ const findBeer = (beer) => {
     // header.appendChild(beerCard);
 };
 
+getBeer();  
 
-getBeer();
 
-console.log(searchResults);    
+fetch("https://api.punkapi.com/v2/beers/random")
+    .then((resp) => resp.json())
+    .then((data) => {
+        //console.log(data);
+        data.forEach(rand => {
+            randomBeer(rand);
+        });
+    });
+
+
+const randomBeer = (rand) => {
+    const beerImg = document.createElement("img");
+    const beerName = document.createElement("h4");
+    const beerAbv = document.createElement("p");
+    const beerTagLine = document.createElement("p");
+    const beerContainer = document.createElement("div");
+    const beerCard = document.createElement("div");
+    beerCard.classList.add("card");
+    beerContainer.classList.add("container");
+    beerImg.classList.add("beerPhoto");
+
+
+    beerImg.src = rand.image_url;
+    beerImg.alt = `A picture of ${rand.name}`;
+    beerAbv.innerText = "ABV: " + rand.abv + "%";
+    beerTagLine.innerText = rand.tagline;
+    beerName.innerText = rand.name;
+
+    randomButton.addEventListener("click", () => {
+            beerContainer.appendChild(beerName);
+            beerContainer.appendChild(beerAbv);
+            beerContainer.appendChild(beerTagLine);
+            beerContainer.appendChild(beerImg);
+            beerCard.appendChild(beerContainer);
+            header.appendChild(beerCard);
+    })
+}
+
+
+console.log(searchResults);  
