3c99c17f1721561592b631986a5028c0588a275b
Austin Byrd <byrda05@gmail.com>
2023-10-18 12:30:36 -0500

    update search bar reset, search bar case issue, remove spacing, remove consol-logs, changed card container p tag font size

--- js/index.js
+++ js/index.js
@@ -1,15 +1,11 @@
 let searchTerm = document.getElementById("form-search");
-const searchButton = document.querySelector("form");
+const searchButton = document.getElementById("form");
 let searchResults = [];
 
 const randomButton = document.getElementById("random");
 const resetButton = document.getElementById("reset");
 let display = document.querySelector(".display");
 const header = document.getElementById("header-background");
-
-
-
-
 
 const removeChildren = () => {
     let firstChild = display.firstChild;
@@ -26,10 +22,10 @@ const getBeer = () => {
     fetch("https://api.punkapi.com/v2/beers?page=2&per_page=80")
         .then((resp) => resp.json())
         .then((data) => {
-            console.log(data);
             data.forEach(x => {
                 findBeer(x);
             });
+            searchButton.reset();
         });
     
 };
@@ -53,10 +49,11 @@ const findBeer = (beer) => {
     beerTagLine.innerText = beer.tagline;
     beerName.innerText = beer.name;
 
-    let text = String(beerTagLine.innerText);
-    
+    const beerTagLineLower = beer.tagline.toLowerCase();
+    const searchTermLower = searchTerm.value.toLowerCase();
+    let text = String(beerTagLineLower);
 //search button works
-    let test = text.indexOf(searchTerm.value);
+    let test = text.indexOf(searchTermLower);
     if (test != -1) {
         beerContainer.appendChild(beerName);
         beerContainer.appendChild(beerAbv);
@@ -64,30 +61,17 @@ const findBeer = (beer) => {
         beerContainer.appendChild(beerImg);
         beerCard.appendChild(beerContainer);
         display.appendChild(beerCard);
-    } else {
-        console.log("does not exist");
-    }; 
+    } 
 };
-
-
-
-
 
 searchButton.addEventListener("submit", (e) => {
     e.preventDefault();
     display.remove();
-    console.log(display);
     display = document.createElement("div")
     display.classList.add("display");
     header.appendChild(display);  
     getBeer();  
-    
  })
-
-
-
-
-
 
 const getRandomBeer = () => {
 fetch("https://api.punkapi.com/v2/beers/random")
@@ -110,14 +94,14 @@ const randomBeer = (rand) => {
     beerContainer.classList.add("container");
     beerImg.classList.add("beerPhoto");
 
-
+    if (rand.image_url === null) {
+        rand.image_url = "https://images.punkapi.com/v2/keg.png";
+    }
     beerImg.src = rand.image_url;
     beerImg.alt = `A picture of ${rand.name}`;
     beerAbv.innerText = "ABV: " + rand.abv + "%";
     beerTagLine.innerText = rand.tagline;
     beerName.innerText = rand.name;
-
-
     beerContainer.appendChild(beerName);
     beerContainer.appendChild(beerAbv);
     beerContainer.appendChild(beerTagLine);
@@ -126,31 +110,21 @@ const randomBeer = (rand) => {
     display.appendChild(beerCard);
 }
 
-
 randomButton.addEventListener("click", () => {
     display.remove();
-    console.log(display);
     display = document.createElement("div")
     display.classList.add("display");
     header.appendChild(display); 
     getRandomBeer();
 })
 
-
-
-
-
-
 resetButton.addEventListener("click", () => {
     removeChildren();
 })
 
-
 document.addEventListener("DOMContentLoaded", function() {
     let checkbox = document.getElementById("toggle");
     const audio = new Audio("audio/getbeer.mp3");
-   
-  
     checkbox.addEventListener("change", function() {
         var style = document.getElementById('style');
         if (checkbox.checked) {
@@ -158,7 +132,6 @@ document.addEventListener("DOMContentLoaded", function() {
             audio.volume = 0.1;
             audio.loop = true;
       
-            
             const volumeControl = document.createElement("input");
             volumeControl.setAttribute("type", "range");
             volumeControl.setAttribute("id", "volume-control"); 
@@ -169,7 +142,6 @@ document.addEventListener("DOMContentLoaded", function() {
             const footer = document.querySelector("footer"); 
             footer.appendChild(volumeControl);
       
-            
             const volume = document.querySelector("#volume-control");
             volume.addEventListener("change", function(e) {
                 const adjustedVolume = e.currentTarget.value / 100; 
@@ -187,3 +159,6 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     });
   });
+searchTerm.addEventListener("click", (e) =>{
+    console.log("hello");
+})
